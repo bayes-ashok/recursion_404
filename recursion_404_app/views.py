1a6d08f4d207bc71484dc7ac7be12c6f443c4c17
Nikesh <bhandarinikesh93@gmail.com>
2023-12-26 02:36:50 +0545

    views

--- recursion_404_app/views.py
+++ recursion_404_app/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def index(request):
     return render(request,'report.html')
+
+def fixed(request):
+    return render(request,'fixed.html')
+
+def security(request):
+    return render(request,'security.html')
+
+def signup(request):
+    return render(request,'signup.html')
