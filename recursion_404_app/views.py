ab17b6c88d08c4019fae955ec849feba4b03eb7c
Ashok Shah <121223930+bayes-ashok@users.noreply.github.com>
2023-12-25 20:41:33 +0545

    changes in config

--- recursion_404_app/views.py
+++ recursion_404_app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, this is myapp!")
+    return render(request,'report.html')
