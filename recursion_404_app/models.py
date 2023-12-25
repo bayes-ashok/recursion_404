df4329456160306e26be617ec98326aaeb510474
FIRST_NAME LAST_NAME <ayush.ad741@gmail.com>
2023-12-26 03:45:46 +0545

    model made for report

--- recursion_404_app/models.py
+++ recursion_404_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='location_images/')
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
