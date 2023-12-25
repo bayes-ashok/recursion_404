e6d473d34a6ca5d1ac1365702b3db7f238741989
Nikesh <bhandarinikesh93@gmail.com>
2023-12-26 02:37:09 +0545

    urls

--- recursion_404_app/urls.py
+++ recursion_404_app/urls.py
@@ -1,8 +1,15 @@
 
 
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('', views.index,name='index'),
+    path('fixed', views.fixed,name='fixed'),
+    path('security', views.security,name='security'),
+    path('signup', views.signup,name='signup'),
+
+    
+    
 ]
