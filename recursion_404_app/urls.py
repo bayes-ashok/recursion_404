d9c0cb8678144b6d4d87988246289cc71fd63db7
FIRST_NAME LAST_NAME <ayush.ad741@gmail.com>
2023-12-26 07:09:41 +0545

    urls

--- recursion_404_app/urls.py
+++ recursion_404_app/urls.py
@@ -7,5 +7,6 @@ from . import views
 urlpatterns = [
     path('', views.index,name='index'),
     path('report_pothole', views.report_pothole, name='report_pothole'),
-    path('login',views.User,name="login")
+    path('signup',views.signup,name="signup"),
+    path('login',views.login,name="login"),
 ]
