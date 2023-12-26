3300346cd714b15d6717dc4a99063955613c856d
FIRST_NAME LAST_NAME <ayush.ad741@gmail.com>
2023-12-26 07:10:22 +0545

    models

--- recursion_404_app/models.py
+++ recursion_404_app/models.py
@@ -12,40 +12,12 @@ class Location(models.Model):
 
     def __str__(self):
         return self.name
-    
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, phone_number, password=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, phone_number=phone_number, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, phone_number, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        return self.create_user(email, phone_number, password, **extra_fields)
-class User(AbstractBaseUser, PermissionsMixin):
-    user_id = models.AutoField(primary_key=True)
+class Signup(models.Model):
     name = models.CharField(max_length=1000)
     email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=15, unique=True, null=True)  # Assuming a maximum length of 15 for phone numbers
+    phone_number = models.CharField(max_length=10, unique=True, null=True) 
     password = models.CharField(max_length=128)
-    # latlong values
- 
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-
-    objects = CustomUserManager()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name', 'phone_number']
-
-    # Add or change related_name for groups and user_permissions
-    groups = models.ManyToManyField('auth.Group', related_name='custom_user_groups', blank=True)
-    user_permissions = models.ManyToManyField('auth.Permission', related_name='custom_user_permissions', blank=True)
 
     def __str__(self):
-        return self.email
+        return self.name
+
